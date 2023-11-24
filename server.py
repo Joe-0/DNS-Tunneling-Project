a7a49b543fc23f118ae81cbec326c65660e5168f
Joe <jstearns@iwu.edu>
2023-11-24 17:17:11 -0600

    Newer code to test if server can recieve code

--- server.py
+++ server.py
@@ -6,9 +6,9 @@ import socket
 from dnslib import *
 
 HOST = "0.0.0.0"  # Standard loopback interface address (localhost)
-PORT = 53  # Port to listen on (non-privileged ports are > 1023)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.bind((HOST, PORT))
     s.listen()
     conn, addr = s.accept()
