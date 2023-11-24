296447412c9d8d9318e2d2b68a7e182d273c9c6e
Joe <jstearns@iwu.edu>
2023-11-24 17:12:11 -0600

    Code should be sending DNS requests to 8.8.8.8 correctly now

--- client.py
+++ client.py
@@ -6,30 +6,39 @@ import socket
 from dnslib import *
 
 #HOST = "54.225.14.129" IP of server
-HOST = "joe.unsatisfiable.net"  # The server's hostname or IP address (will need to be domain name)
-#HOST = "8.8.8.8"
-PORT = 53  # The port used by the server
+URL = "joe.unsatisfiable.net"  # The server's hostname or IP address (will need to be domain name)
+DNS_HOST = "8.8.8.8"
+PORT = 65432  # The port used by the server
 #PORT = 1024
 
 
 print("TEST")
 
-user_input = input("Please enter then data to send: ")
+'''user_input = input("Please enter then data to send: ")
 query = DNSRecord.question(user_input + "." + HOST)
-print(query)
 
+print(query)'''
 
-'''with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
+# Had to change SCOK_STREAM to SOCK_DGRAM to fix broken pipe error
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     while True:
-        print("HERE")
         user_input = input("Please enter then data to send: ")
-        query = DNSRecord.question(user_input + "." + HOST)
+        query = DNSRecord.question(user_input + "." + URL)
+
         print(query)
+        #.pack to return bytes object can not send a DNSRecord
+        s.sendto(query.pack(), (DNS_HOST, PORT)) # need to send the info to a DNS server for this it is google
+        #s.sendall(b"\n")
+        #print("HERE")
+        #user_input = input("Please enter then data to send: ")
+        #query = DNSRecord.question(user_input + "." + HOST)
+        #print(query)
         #s.sendall(user_input.encode('uft-8') + "." + HOST)
-        s.sendall(b"\n")
+        #s.sendall(b"\n")
         if user_input == "":
             break
-    data = s.recv(1024)'''
+        data = s.recv(1024)
 
-#print(f"Received {data!r}")
+    s.close()
+
+print(f"Received {data!r}")
