c6e5a1c01667b2e209842c84a46319a30eed7178
Joe <jstearns@iwu.edu>
2023-11-25 13:52:52 -0600

    Changed code to send data using UDP

--- simple_client.py
+++ simple_client.py
@@ -4,12 +4,14 @@
 
 import socket
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
+HOST = "54.225.14.129"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    msg = (b"Hello, world")
 
-print(f"Received {data!r}")
+    s.sendto(msg, (HOST, PORT))
+
+    data, addr = s.recvfrom(1024)
+
+print(f"Received {data.decode('utf-8')} from {addr}")
